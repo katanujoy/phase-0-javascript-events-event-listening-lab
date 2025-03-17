b8802406d6918a87cc0c88825ad378b3f79120eb
katanujoy <katanujoyy99@gmail.com>
2025-03-17 16:02:58 +0300

    Added event listener functionality

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
-  
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-  
-    input.addEventListener('click', clickAlert);
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
   }
-  
-  addingEventListener(); 
-  
+
+  input.addEventListener('click', clickAlert);
+}
+
+// Call the function to activate the event listener
+addingEventListener();
